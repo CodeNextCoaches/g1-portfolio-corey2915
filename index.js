25450516e86c9b6c0cd9a5343d9fb671cc679422
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-01-27 12:41:26 -0500

    hi

---
+++ index.js
@@ -0,0 +1,16 @@
+function setup(){
+createCanvas(1400,250);
+}
+
+function draw(){
+  welcome();
+}
+
+function welcome(){
+  var s = 'welcome to corey profile';
+  fill(50);
+  textSize(100)
+  text(s,100,100); // Text wraps within text box
+
+
+}
